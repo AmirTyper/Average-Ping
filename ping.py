6be0c15e5c19264d228164a2cf2731653d1771af
AmirHossein Abdollahi <amir.xx.typer@gmail.com>
2023-03-08 20:15:54 +0330

    Create ping.py

---
+++ ping.py
@@ -0,0 +1,21 @@
+import subprocess
+import re
+from termcolor import cprint
+
+x = input("Write the Domain or IP: ")
+
+print("Testing...")
+
+try:
+    output = subprocess.check_output("ping "+ x, shell = False, universal_newlines=True).splitlines()
+    for i in output:
+        if "Packets" in i:
+            var1 = int(re.search(r'\d+', str(re.findall(r'Lost =\s\d*',i))).group())
+            cprint("Packet lost: {0}".format(var1),"red")
+        if "Minimum" in i:
+            var2 = int(re.search(r'\d+', str(re.findall(r'Average =\s\d*',i))).group())
+            cprint("Average ms: {0}".format(var2),"green")
+except:
+    cprint("Error!","red")
+
+
